be1212f6ee5b10af2bd3a37dfa182710620729da
Youssef Elgendy <youssef.elgendy477@gmail.com>
2025-07-03 13:13:07 +0300

    added data of categoty tapview

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color grey = Colors.grey;
+  static const Color primary = Colors.deepPurple;
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color green = Colors.green;
+  static const Color blue = Colors.blue;
 }
--- lib/view/widgets/category_tap_view.dart
+++ lib/view/widgets/category_tap_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/models/category_model.dart';
 import 'package:flutter/material.dart';
 
 class CategoryTapView extends StatelessWidget {
@@ -5,6 +6,47 @@ class CategoryTapView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Category'));
+    return ListView.builder(
+      itemCount: dummyCategory.length,
+      itemBuilder: (BuildContext context, int index) {
+        final category = dummyCategory[index];
+        return InkWell(
+          onDoubleTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.0),
+                color: category.bgColor,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 32,
+                  horizontal: 16,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      category.name,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: category.textColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '${category.productsCount} Products',
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                        color: category.textColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
